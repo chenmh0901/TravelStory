6974ba322a8a24b9de5ed35b5441ea5c6580bb68
chenmh0901 <1210204146@zust.edu.cn>
2024-03-29 04:58:28 -0400

    update header => navigator

--- src/components/navigator.vue
+++ src/components/navigator.vue
@@ -1,26 +1,28 @@
 <script setup lang="ts">
 import { UserOutlined, SearchOutlined } from '@ant-design/icons-vue';
+import logo from '@/assets/logo.png';
 const onSearch = () => {
   console.log('search');
 };
 </script>
 
 <template>
-  <header class="header">
-    <div class="logo" />
-    <h1 class="text-black text-xl">TravelStories</h1>
+  <header class="navigator">
+    <router-link to="/">
+      <div class="logo">
+        <img :src="logo" alt="logo" class="w-[30px] h-[30px]" />
+        <h1 class="text-black text-xl">旅行日记</h1>
+      </div>
+    </router-link>
     <a-input class="w-[250px] mx-4 text-start" placeholder="Search">
       <template #prefix><SearchOutlined @click="onSearch" /></template>
     </a-input>
     <div class="menu">
-      <router-link :to="{ path: '/', query: { v: 'cards' } }">
-        <a-button class="menu_btn" type="text">Cards</a-button></router-link
-      >
-      <router-link :to="{ path: '/', query: { v: 'map' } }">
-        <a-button class="menu_btn" type="text">Map</a-button></router-link
+      <router-link :to="{ path: '/', query: { v: 'stories' } }">
+        <a-button class="menu_btn" type="text">日记</a-button></router-link
       >
       <router-link :to="{ path: '/upload' }"
-        ><a-button class="menu_btn" type="text">Upload</a-button>
+        ><a-button class="menu_btn" type="text">上传</a-button>
       </router-link>
     </div>
     <a-avatar :size="36">
@@ -30,17 +32,16 @@ const onSearch = () => {
 </template>
 
 <style scoped>
-.header {
-  @apply fixed top-0 w-full z-10 flex gap-6 items-center justify-center text-white  bg-white;
+.navigator {
+  @apply fixed top-0 w-full z-10 flex gap-12 items-center justify-center text-white  bg-white;
 }
 .menu {
-  @apply h-[50px] flex items-center gap-6 ml-20 mr-5;
+  @apply h-[50px] flex items-center gap-6 mr-5;
 }
 .menu_btn {
   @apply text-black font-bold h-[35px] border-0 rounded-full hover:bg-gray-300;
 }
 .logo {
-  @apply w-[120px] h-[31px] mr-1.5;
-  background: rgba(110, 110, 255, 0.2);
+  @apply flex flex-row gap-1 mr-1.5;
 }
 </style>
