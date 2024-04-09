c3b0cace815ffaecf7f4bc39002901ce3cb0b155
chenmh0901 <1210204146@zust.edu.cn>
2024-04-09 03:19:32 -0400

    update: temp routestone

--- src/components/map/index.vue
+++ src/components/map/index.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
-import { mountMap } from 'traveler-map';
+import { mountMap, cameraMoveTo } from 'traveler-map';
 
-onMounted(() => mountMap('map'));
+const move = () => {
+  cameraMoveTo(30, 120, 13);
+}
+
+const initMap = () => {
+  mountMap('map')
+}
+
+onMounted(initMap);
 </script>
 
 <template>
   <div id="map"></div>
+  <button @click="move" />
 </template>
 
 <style scoped>
